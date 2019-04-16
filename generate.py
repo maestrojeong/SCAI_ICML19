50dd5ae4b517c2e2bb6906aee3e2e16c7e5ea297
maestrojeong <yeonwoo@mllab.snu.ac.kr>
2019-04-17 00:22:44 +0900

    first commit

---
+++ generate.py
@@ -0,0 +1,5 @@
+
+for idx in range(10):
+    print("\only<%d>{\includegraphics[width=0.9textwidth,center]{vae_gif/vae-%d.png}}"%(idx+1, 5*idx))
+
+#\only<50>{\includegraphics[width=0.9\textwidth,center]{factor_gif/factor-49.png}}
